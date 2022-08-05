f4a513f32019bf8976bb723496b5333eb06b6e5f
cedrata <81557163+cedrata@users.noreply.github.com>
2022-08-05 21:46:03 +0200

    [UPDATE] schema in initialization script.

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -9,27 +9,33 @@ db = connect('mongodb://root:root@localhost:27017');
 db = db.getSiblingDB('fastapi_auth_template')
 
 db.createUser(
-    {
-        user: DB_ADMIN_USERNAME,
-        pwd: DB_ADMIN_PASSWORD,
-        roles: [{ role: 'dbOwner', db: 'fastapi_auth_template'}]
-    }
+	{
+		user: DB_ADMIN_USERNAME,
+		pwd: DB_ADMIN_PASSWORD,
+		roles: [{ role: 'dbOwner', db: 'fastapi_auth_template' }]
+	}
 )
 
 db.createCollection('users')
 db.users.insertMany(
-    [
-        {
-            username: 'admin',
-            password: '$2b$12$N/LPnzvpHyE2KI2cuxhMz.3FSnF7MuoN6EeDKtE9yGiqMBVj3US/e',
-            roles: []
-        },
-        {
-            username: 'user',
-            password: '$2b$12$bCT0LidMlwjjA1YCKtZkxeuc74CU1R1zxqE9ntSE7s7IYkP2fbtrK',
-            roles: []
-        }
-    ]
+	[
+		{
+			email: 'admin@email.com',
+			username: 'admin',
+			password: '$2b$12$N/LPnzvpHyE2KI2cuxhMz.3FSnF7MuoN6EeDKtE9yGiqMBVj3US/e',
+			roles: ['admin'],
+			"creation": new Date("2022-08-05T17:35:00.060Z"),
+			"last_update": new Date("2022-08-05T17:35:00.060Z"),
+		},
+		{
+			email: 'user@email.com',
+			username: 'user',
+			password: '$2b$12$bCT0LidMlwjjA1YCKtZkxeuc74CU1R1zxqE9ntSE7s7IYkP2fbtrK',
+			roles: ['user'],
+			"creation": new Date("2022-07-15T21:37:00.000Z"),
+			"last_update": new Date("2022-07-15T21:37:00.000Z"),
+		}
+	]
 )
 
 print('END ###################################################################');
