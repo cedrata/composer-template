d752c72fb909dcf8e43032d1a6a21b1b0051338c
cedrata <81557163+cedrata@users.noreply.github.com>
2022-08-21 14:52:21 +0200

    [UPDATE] replace subtrees with submodules.

--- api/api/src/models/auth.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class AuthMessage(BaseModel):
-    """Class that represent the answer after a succesful authentication."""
-
-    access_token: str
-    refresh_token: str
-    token_type: str
